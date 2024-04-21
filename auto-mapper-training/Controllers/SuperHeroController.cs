640360d89a35e7fd1ee544bcfd094135547ed134
Weiss Jiang <meweissjiang@gmail.com>
2024-04-21 18:17:22 +1000

    created training project for dotnet

---
+++ auto-mapper-training/Controllers/SuperHeroController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace automappertraining
+{
+    [Route("api/training/[controller]")]
+    [ApiController]
+    public class SuperHeroController: ControllerBase
+    {
+    }
+}
